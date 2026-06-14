e1b1b6bf91d221b73c92aaedcc93fb7730f4db29
svnty <jakewalklate@gmail.com>
2026-06-14 23:11:28 +1000

    configure capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'rust-neon-next-app',
-  webDir: 'public'
+  appName: 'rust-napi-next-app',
+  webDir: 'out',
+  server: {
+    url: 'http://localhost:3000',
+    cleartext: !isProduction
+  }
 };
 
 export default config;
