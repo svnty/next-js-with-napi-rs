b2fa929a8d984b0572cda222dc6864b7a475c77a
svnty <jakewalklate@gmail.com>
2026-06-14 21:16:20 +1000

    .

---
+++ dist/server/chunks/_next-internal_server_app_api_rust-test_route_actions_0ofp74y.js
@@ -0,0 +1,3 @@
+module.exports=[95697,(e,o,d)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_api_rust-test_route_actions_0ofp74y.js.map
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   serverExternalPackages: ["rust-lib"],
+  distDir: "dist",
 };
 
 export default nextConfig;
